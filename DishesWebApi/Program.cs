91cf3077cc149066a8787799885630a19a61db0c
Neven Dimač <neven.dimac@gmail.com>
2025-02-09 09:29:38 +0100

    Added two new routes and added route contraint

--- DishesWebApi/Program.cs
+++ DishesWebApi/Program.cs
@@ -22,6 +22,23 @@ app.MapGet("/dishes", async (DishesDbContext context) =>
     return await context.Dishes.ToListAsync();
 });
 
+app.MapGet("/dishes/{dishId:guid}", async (DishesDbContext context, Guid dishId) =>
+{
+    return await context.Dishes.FirstOrDefaultAsync(d => d.Id == dishId);
+});
+
+app.MapGet("/dishes/{dishName}", async (DishesDbContext context, string dishName) =>
+{
+    return await context.Dishes.FirstOrDefaultAsync(d => d.Name == dishName);
+});
+
+app.MapGet("/dishes/{dishId}/ingredients", async (DishesDbContext context, Guid dishId) =>
+{
+    return (await context.Dishes
+    .Include(d => d.Ingredients)
+    .FirstOrDefaultAsync(d => d.Id == dishId))?.Ingredients;
+});
+
 // recreate & migrate the database on each run
 using (var serviceScope = app.Services?.GetService<IServiceScopeFactory>()?.CreateScope())
 {
