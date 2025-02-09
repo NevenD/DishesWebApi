5aab6ddc79c49249cd168f4fade28d08e8578730
Neven Dimač <neven.dimac@gmail.com>
2025-02-09 09:13:23 +0100

    Added dishes get route with async await

--- DishesWebApi/Program.cs
+++ DishesWebApi/Program.cs
@@ -15,22 +15,11 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapGet("/dishes", async (DishesDbContext context) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return await context.Dishes.ToListAsync();
 });
 
 // recreate & migrate the database on each run
@@ -42,7 +31,4 @@ using (var serviceScope = app.Services?.GetService<IServiceScopeFactory>()?.Crea
 }
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
