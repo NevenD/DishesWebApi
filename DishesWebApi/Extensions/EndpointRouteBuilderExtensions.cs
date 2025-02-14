2db8c0ef65266923a2681ead921fa83a9e0aa341
Neven Dimač <neven.dimac@gmail.com>
2025-02-14 17:48:13 +0100

    Added dish filter middleware

--- DishesWebApi/Extensions/EndpointRouteBuilderExtensions.cs
+++ DishesWebApi/Extensions/EndpointRouteBuilderExtensions.cs
@@ -14,7 +14,26 @@ namespace DishesWebApi.Extensions
             dishesWithGuidIdEndPoints.MapGet("", DishesHandlers.GetDishesByIdAsync).WithName("GetDish");
             dishesEndPoints.MapGet("/{dishName}", DishesHandlers.GetDishesByNameAsync);
             dishesEndPoints.MapPost("", DishesHandlers.CreateDishAsync);
-            dishesWithGuidIdEndPoints.MapPut("", DishesHandlers.UpdateDishAsync);
+            dishesWithGuidIdEndPoints.MapPut("", DishesHandlers.UpdateDishAsync)
+                .AddEndpointFilter(async (context, next) =>
+                {
+                    var dishId = context.GetArgument<Guid>(2);
+                    var rendangId = new Guid("fd630a57-2352-4731-b25c-db9cc7601b16");
+
+                    if (dishId == rendangId)
+                    {
+                        return TypedResults.Problem(new()
+                        {
+                            Status = StatusCodes.Status400BadRequest,
+                            Title = "Rendang is not allowed to be updated",
+                            Detail = "Rendang is a national dish of Indonesia and should not be updated"
+                        });
+                    }
+                    // invoke the next filter in the pipeline
+                    var result = await next.Invoke(context);
+
+                    return result;
+                });
             dishesWithGuidIdEndPoints.MapDelete("", DishesHandlers.DeleteDishAsync);
 
         }
